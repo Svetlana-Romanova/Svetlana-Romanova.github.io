936ff926064fb5a3b6b12f837876cc70cb589d84
Svetlana <svetlanakrbva@rambler.ru>
2020-05-28 18:46:18 +0300

    большие правки после ревью

--- js/main.js
+++ js/main.js
@@ -1,40 +1,48 @@
 'use strict';
 
 var hamburger = document.querySelector('.logo__hamburger');
-var nav = document.querySelector('.nav__list');
+var nav = document.querySelector('.nav');
+var navList = document.querySelector('.nav__list');
 var navItem = document.querySelectorAll('.nav__item');
 var navLink = document.querySelectorAll('.nav__link');
-var logoImg = document.querySelector('.logo__img');
 var logoHamburger = document.querySelector('.logo__hamburger');
+
 
 function ready() {
   function openHamburger() {
     hamburger.classList.toggle('logo__hamburger--open');
 
-    nav.classList.toggle('nav__list--adaptiv');
+    navList.classList.toggle('nav__list--adaptive');
 
     navItem.forEach(function (item) {
-      if (item.classList.contains !== ('nav__item--adaptiv')) {
-        item.classList.add('nav__item--adaptiv');
+      if (item.classList.contains !== ('nav__item--adaptive')) {
+        item.classList.add('nav__item--adaptive');
       }
     });
 
     navLink.forEach(function (item) {
-      if (item.classList.contains !== ('nav__link--adaptiv')) {
-        item.classList.add('nav__link--adaptiv');
+      if (item.classList.contains !== ('nav__link--adaptive')) {
+        item.classList.add('nav__link--adaptive');
       }
     });
   }
 
   function closeHamburger() {
     hamburger.classList.remove('logo__hamburger--open');
-    nav.classList.remove('nav__list--adaptiv');
+    navList.classList.remove('nav__list--adaptive');
   }
 
-  nav.classList.remove('nav__list--nojs');
-  logoImg.style.padding = '20px';
+  function deleteNojs() {
+    nav.classList.remove('nav--nojs');
+    navList.classList.remove('nav__list--nojs');
+    navItem.forEach(function (item) {
+      item.classList.remove('nav__item--nojs');
+    });
+  }
 
-  logoHamburger.classList.add('logo__hamburger--adaptiv');
+  deleteNojs();
+
+  logoHamburger.classList.add('logo__hamburger--adaptive');
 
   hamburger.addEventListener('click', openHamburger);
 
@@ -62,6 +70,16 @@ function ready() {
       }
     }
   });
+
+  var custom2 = new Scrollbot('#body', 10).setStyle({
+    'background': 'none',
+    'z-index': '2'
+  }, {
+    'background': 'rgba(0,0,0,0)'
+  });
+  var psuedo = document.createElement('div');
+  psuedo.style.cssText = 'height:100%;width:2px;left:4px;background:#808080;position:absolute;z-index:1';
+  custom2.scrollBarHolder.appendChild(psuedo);
 }
 
 document.addEventListener('DOMContentLoaded', ready);
